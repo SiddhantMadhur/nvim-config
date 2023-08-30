040d734858c44c1cec0bed0585c5a74cabab6a5e
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-08-30 10:39:17 +0400

    Updated Treesitter to support indenting .jsx properly

--- lua/siddhantmadhur/packer.lua
+++ lua/siddhantmadhur/packer.lua
@@ -6,13 +6,11 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
-
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.2',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-
 	use({
 		'rose-pine/neovim',
 		as = 'rose-pine',
@@ -20,14 +18,10 @@ return require('packer').startup(function(use)
 			vim.cmd('colorscheme rose-pine')
 		end
 	})
-
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
 	use('theprimeagen/harpoon')
-
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
-
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v2.x',
@@ -43,5 +37,4 @@ return require('packer').startup(function(use)
 			{'L3MON4D3/LuaSnip'},     -- Required
 		}
 	}
-
 end)
