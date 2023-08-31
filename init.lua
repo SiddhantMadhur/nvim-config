38297506d9f59fd77fec42365e3a91851969590c
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-08-31 14:11:37 +0400

    Indentation: 2 spaces intead of 4

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("siddhantmadhur")
 	local set = vim.opt
-	set.tabstop = 4
-	set.softtabstop = 4
-	set.shiftwidth = 4
+	set.tabstop = 2
+	set.softtabstop = 2
+	set.shiftwidth = 2
+	set.number = true
