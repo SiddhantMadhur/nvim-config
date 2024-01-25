60ab5ed14e5b39c9f5d8821726d07c324c7d78f4
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-01-24 18:32:13 -0800

    New year new setup

---
+++ lua/siddhantmadhur/init.lua
@@ -0,0 +1,7 @@
+require('siddhantmadhur.remap')
+vim.opt.termguicolors = true
+vim.cmd.colorscheme 'melange'
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
