9d18827e576543c5906a4a9eab75e06b6390ce13
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-02-16 14:45:37 -0800

    Added Svelte Lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,23 +10,23 @@ local lspconfig = require('lspconfig')
 -- here you can setup the language servers
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { 'emmet_ls', 'clangd', 'rust_analyzer', 'tailwindcss', 'gopls', 'templ', 'tsserver', 'htmx', 'html'},
+  ensure_installed = {'svelte', 'emmet_ls', 'clangd', 'rust_analyzer', 'tailwindcss', 'gopls', 'templ', 'tsserver', 'htmx', 'html'},
   handlers = {
     lsp_zero.default_setup,
     lspconfig.html.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        filetypes = {"html", "templ"}
+        filetypes = {"html", "svelte", "templ"}
     }),
     lspconfig.emmet_ls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        filetypes = {"html", "templ"}
+        filetypes = {"html", "templ", "svelte"}
     }),
     lspconfig.tailwindcss.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        filetypes = {"html", "templ"}
+        filetypes = {"html", "templ", "svelte"}
     }),
     lspconfig.htmx.setup({
         on_attach = on_attach,
