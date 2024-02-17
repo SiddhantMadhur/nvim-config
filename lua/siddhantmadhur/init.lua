43ad80721c325f468802b9cf5cde450c3a17b015
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-02-17 14:37:26 -0800

    Added bg-transparency to cfg

--- lua/siddhantmadhur/init.lua
+++ lua/siddhantmadhur/init.lua
@@ -1,5 +1,10 @@
 require('siddhantmadhur.remap')
 vim.cmd("colorscheme kanagawa")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
